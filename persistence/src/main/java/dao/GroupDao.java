c2283d32bedc01858e26b760496e939e1b01493e
AdrianaCatoiu <adriana.catoiu@yahoo.com>
2015-09-17 12:19:58 +0300

    GroupDao, IGroupDao

--- persistence/src/main/java/dao/GroupDao.java
+++ persistence/src/main/java/dao/GroupDao.java
@@ -1,7 +1,79 @@
 package dao;
 
+import model.Group;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by DELIA on 17.09.2015.
  */
-public class GroupDao {
+public class GroupDao implements IGroupDao {
+
+
+    private EntityManager entityManager;
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public List<Group> getAll(){
+
+        return this.entityManager.createQuery("from Group").getResultList();
+    }
+
+    public Group getGroup(Long id)
+    {
+        if(id == null){
+            return null;
+        }
+        else
+        {
+            return entityManager.find(Group.class, id);
+        }
+    }
+
+    @Transactional
+    public void saveGroup(Group group)
+    {
+        group.setStartDate(new Date());
+        entityManager.persist(group);
+
+    }
+
+    @Transactional
+    public void updateGroup(Group group)
+    {
+
+        Group groupFromDbs = this.getGroup(group.getId());
+        if (groupFromDbs != null)
+        {
+            groupFromDbs.setName(group.getName());
+            groupFromDbs.setStartDate(group.getStartDate());
+            groupFromDbs.setEndDate(group.getEndDate());
+            entityManager.persist(groupFromDbs);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- persistence/src/main/java/dao/IGroupDao.java
+++ persistence/src/main/java/dao/IGroupDao.java
@@ -1,7 +1,21 @@
 package dao;
 
+import model.Group;
+
+import java.util.List;
+
 /**
  * Created by DELIA on 17.09.2015.
  */
 public interface IGroupDao {
+
+
+    public List<Group> getAll();
+    public void saveGroup(Group group);
+    public void updateGroup(Group group);
+    public void deleteGroup(Long id);
+    public Group getGroup(Long id);
+
+
+
 }
