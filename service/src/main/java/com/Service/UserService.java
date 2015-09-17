3c8f71bc6bccfe10abec9cecf952a1e9eac697dc
Eddy028 <eduard.george028@gmail.com>
2015-09-17 12:08:44 +0300

    update

--- service/src/main/java/com/Service/UserService.java
+++ service/src/main/java/com/Service/UserService.java
@@ -1,7 +1,60 @@
-package com.Service;
+package com.service;
+
+import facade.IUserFacade;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Eddy on 9/17/2015.
+ * Created by root on 9/10/15.
  */
+@Controller
+@RequestMapping("/booyah/api/user")
 public class UserService {
+
+    @Autowired
+    private IUserFacade userFacade;
+
+    @RequestMapping(value="", method= RequestMethod.GET)
+    @ResponseBody
+    public List<User> getAllUser() {
+        return this.userFacade.bringUsers();
+    }
+
+    @RequestMapping(value="/{userId}", method= RequestMethod.GET)
+    @ResponseBody
+    public User getUser(@PathVariable("userId") Long userId) {
+        return this.userFacade.geUser(userId);
+    }
+
+    @RequestMapping(value="/{userId}", method= RequestMethod.PUT)
+    @ResponseBody
+    public void updateUser(@PathVariable("userId")Long userId, @RequestBody User userUpdates) {
+        userUpdates.setId(userId);
+        this.userFacade.updateUser(userUpdates);
+    }
+
+    @RequestMapping(value="", method= RequestMethod.POST)
+    @ResponseBody
+    public void addUser(@RequestBody User newUser){
+        this.userFacade.saveUser(newUser);
+    }
+
+    @RequestMapping(value="/{userId}", method= RequestMethod.DELETE)
+    @ResponseBody
+    public void deleteUser(@PathVariable("userId")Long userId){
+        this.userFacade.deleteUser(userId);
+    }
+
+    public IUserFacade getUserFacade() {
+        return userFacade;
+    }
+
+    public void setUserFacade(IUserFacade userFacade) {
+        this.userFacade = userFacade;
+    }
 }
