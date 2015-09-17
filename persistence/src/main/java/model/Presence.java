cbb839e290206d7b4a5a5bb1045ed6ba73359f82
DELIA <delia.axinia@yahoo.com>
2015-09-17 11:50:47 +0300

    presence

--- persistence/src/main/java/model/Presence.java
+++ persistence/src/main/java/model/Presence.java
@@ -30,9 +30,52 @@ public class Presence {
     @Column(name = "date")
     private Date date;
 
+    public Presence() {}
 
+    public Presence(String name, int groupId, int userId, Date date) {
+        this.name = name;
+        this.groupId = groupId;
+        this.userId = userId;
+        this.date = date;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
