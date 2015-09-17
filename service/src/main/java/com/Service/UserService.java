22a2a29d584b219aba99853b7a6b9d0040d48164
AdrianaCatoiu <adriana.catoiu@yahoo.com>
2015-09-17 14:48:27 +0300

    IUserFacade erroor

--- service/src/main/java/com/Service/UserService.java
+++ service/src/main/java/com/Service/UserService.java
@@ -1,12 +1,10 @@
 package com.Service;
 
-import facade.IUserFacade;
 import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
